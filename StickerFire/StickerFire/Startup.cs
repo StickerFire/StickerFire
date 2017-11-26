02c45f3d4a4c98a94b4b6c7db5a0c3f06fe07f5c
drkrieger1 <eplyushko@gmail.com>
2017-11-26 15:48:14 -0800

    exeption handler added

--- StickerFire/StickerFire/Startup.cs
+++ StickerFire/StickerFire/Startup.cs
@@ -59,16 +59,16 @@ namespace StickerFire
                    .AddDefaultTokenProviders();
 
             //Enabel OAuth for Google +
-            services.AddAuthentication().AddGoogle(googleOptions =>
-            {
-                googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
-                googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
-            });
-            services.AddAuthentication().AddFacebook(facebookOptions =>
-            {
-                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
-                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
-            });
+            //services.AddAuthentication().AddGoogle(googleOptions =>
+            //{
+            //    googleOptions.ClientId = Configuration["Authentication:Google:ClientId"];
+            //    googleOptions.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            //});
+            //services.AddAuthentication().AddFacebook(facebookOptions =>
+            //{
+            //    facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+            //    facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            //});
 
         }
 
@@ -80,6 +80,11 @@ namespace StickerFire
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
             app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
 
